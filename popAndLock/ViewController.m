9d2767e7dd580fef397c69bab810a973a8fd4dc1
Robera Geleta <robera.geleta@vensi.com>
2014-08-17 23:05:59 -0500

    implemnted the tapping to scale up the image functionality but still working on fixing some issues with gesture recgonizer conflicts

--- popAndLock/ViewController.m
+++ popAndLock/ViewController.m
@@ -6,17 +6,36 @@
 //  Copyright (c) 2014 EnterWithBoldness. All rights reserved.
 //
 
+#define BIG_SIZE  CGSizeMake(320, 320)
+#define SMALL_SIZE  CGSizeMake(200, 200)
+
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIGestureRecognizerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *image;
 @property CGRect initalImagePosition;
+@property BOOL tapped;
+
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tapGesture;
+
+@property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *panGesture;
 
 @end
 
 @implementation ViewController
 - (IBAction)imageTapped:(UITapGestureRecognizer *)sender
 {
+    
+    NSLog(@"IT just got tapped");
+    POPSpringAnimation *scaleUpImageWithSpringAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerBounds];
+    if (_tapped){
+        scaleUpImageWithSpringAnimation.toValue = [NSValue valueWithCGSize:BIG_SIZE];
+        _tapped = YES;
+    }else{
+        _tapped = NO;
+        scaleUpImageWithSpringAnimation.toValue = [NSValue valueWithCGSize:SMALL_SIZE];
+    }
+
     
 }
 
@@ -25,6 +44,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _tapped = NO;
 
 }
 
@@ -66,21 +86,16 @@
     {
         //check if the view is less than 1/4 of the up from the bottom of the screen
         NSLog(@"%f :%f",self.image.frame.origin.y, (self.view.frame.size.height  - (self.view.frame.size.height /4)));
-        if ( self.image.frame.origin.y >  (self.view.frame.size.height  - (self.view.frame.size.height /4)))
+        if ( self.image.frame.origin.y >  (self.view.frame.size.height  - (self.view.frame.size.height /3)))
         {
             //we know that the user intends to move the image off the screen so slide if off
             //get the velocity and use it to send the view on its way
-          //           [_image pop_addAnimation:animateOutTheViewWithDecay];
-            
-//
-//            POPDecayAnimation *anim = [POPDecayAnimation animationWithPropertyNamed:kPOPLayerPosition];
-//            anim.deceleration = 0.998;
-//            anim.velocity = [NSValue valueWithCGPoint:[recognizer velocityInView:self.view]];
-//            [self.image.layer pop_addAnimation:anim forKey:@"slide"];
-            
             CGPoint velocity = [sender velocityInView:self.view];
+            NSLog(@"velocity: %@", NSStringFromCGPoint(velocity));
+            
             POPDecayAnimation *animateOutTheViewWithDecay = [POPDecayAnimation animationWithPropertyNamed:kPOPViewFrame];
-            animateOutTheViewWithDecay.toValue = [NSValue valueWithCGPoint:velocity];
+            animateOutTheViewWithDecay.deceleration = 0.4;
+            animateOutTheViewWithDecay.velocity = [NSValue valueWithCGPoint:velocity];
             [self.image pop_addAnimation:animateOutTheViewWithDecay forKey:@"moveOut"];
             
 
@@ -99,6 +114,24 @@
         }
         
     }
+    
+}
+
+
+#pragma mark - gesture recognizer delegates
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+
+    if(gestureRecognizer == _tapGesture)
+    {
+        NSLog(@"Tap gesture");
+        return YES;
+    }else
+    {
+        NSLog(@"Pan gesture");
+        return NO;
+    }
+    
     
 }
 
